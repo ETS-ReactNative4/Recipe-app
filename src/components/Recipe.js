7b51c95c4800a7f416f72e584dd84c6be29d4032
flashbackditto <chaunceyberkowits@yahoo.com>
2019-04-10 16:44:05 -0700

    Fixed links and state managment in Recipe.js

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -11,12 +11,27 @@ class Recipe extends React.Component {
     const req = await fetch(`https://www.food2fork.com/api/search?key=${API_KEY}&q=${title}`);
 
     const res = await req.json();
-    console.log(res.recipes[0]);
+    this.setState({ activeRecipe: res.recipes[0]});
+    console.log(this.activeRecipe);
   }
   render() {
-      console.log(this.props);
+    const recipe = this.state.activeRecipe;
     return (
-      <div> Recipe Component </div>
+      <div className="container">
+      { this.state.activeRecipe.length !== 0 &&
+          <div className="active-recipe">
+           <img className="active-recipe__img" src={recipe.image_url} alt ={recipe.title}/>
+           <h3 className="active-recipe__title">{ recipe.title }</h3>
+           <h4 className="active-recipe__publisher">
+            Publisher: <span>{ recipe.publisher }</span>
+           </h4>
+           <p className="active-recipe__website">Website:
+          <span><a href={ recipe.publisher_url }>{ recipe.publisher_url }</a></span>
+           </p>
+           <button className="active-recipe__button">Go Home</button>
+          </div>
+      }
+      </div>
     );
   }
 };
